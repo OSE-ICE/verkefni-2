c6b63fe5f12d13446c5e5c77e1e469e2329b32b0
OSE-ICE <ose1975@gmail.com>
2023-10-03 14:44:09 -0400

    fikt skilaverkefni 2

---
+++ bin/skilaverkefni_2_1.dart
@@ -0,0 +1,11 @@
+  import 'dart:io';
+
+  void main (){
+
+  int sum = 0;
+  for (int i = 1; i <= 10; i++) {
+    print(i);
+    sum += i;
+  }
+  print('Sum of the first 10 natural numbers is $sum.');
+}
