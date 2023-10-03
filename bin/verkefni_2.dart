666ad1d9175b8d946532530362ff0e173a508f55
OSE-ICE <ose1975@gmail.com>
2023-10-03 11:42:54 -0400

    undirbuningur verkefni 2

--- bin/verkefni_2.dart
+++ bin/verkefni_2.dart
@@ -1,5 +1,21 @@
 import 'package:verkefni_2/verkefni_2.dart' as verkefni_2;
+import 'dart:io';
 
-void main(List<String> arguments) {
+  void main(List<String> arguments) {
   print('Hello world: ${verkefni_2.calculate()}!');
 }
+
+  const pizzaPrices = {
+  'margherita': 5.5,
+  'pepperoni': 7.5,
+  'vegetarian': 6.5,
+};
+
+  const order = ['margherita', 'pepperoni'];
+
+  var total = 0.0;
+  for (final elem in order) {
+    total += pizzaPrices[elem]!;
+//total = total + elem;
+}
+print(total);
