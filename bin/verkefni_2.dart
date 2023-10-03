1e1f6c4eeeb85caacb6f67204deb473c037108dd
OSE-ICE <ose1975@gmail.com>
2023-10-03 11:00:30 -0400

    Initial commit

---
+++ bin/verkefni_2.dart
@@ -0,0 +1,5 @@
+import 'package:verkefni_2/verkefni_2.dart' as verkefni_2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${verkefni_2.calculate()}!');
+}
---
+++ test/verkefni_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:verkefni_2/verkefni_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
